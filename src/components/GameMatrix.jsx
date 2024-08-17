ee75dc5de3ff5eea2ceb2198a4ea0f697469d699
Sebastian <romy6047@gmail.com>
2024-08-17 11:49:09 -0600

    disabled checked game cell

--- src/components/GameMatrix.jsx
+++ src/components/GameMatrix.jsx
@@ -16,7 +16,7 @@ export default function GameMatrix({ playerTurns, handlePlayerTurn }) {
             {gameMatrix.map(
                 (row, rowIndex) => <ul key={rowIndex} className='d-flex justify-content-between flex-grow-1 p-0 m-0'>
                     {
-                        row.map((cell, colIndex) => <button key={`${rowIndex}-${colIndex}`} className='w-100 m-1 text-secondary fw-bold display-1' onClick={() => handlePlayerTurn(rowIndex, colIndex)}>
+                        row.map((cell, colIndex) => <button key={`${rowIndex}-${colIndex}`} className='w-100 m-1 text-secondary fw-bold display-1' onClick={() => handlePlayerTurn(rowIndex, colIndex)} disabled={gameMatrix[rowIndex][colIndex] !== null}>
                             &#8288;{gameMatrix[rowIndex][colIndex]}
                         </button>)
                     }
